a088a20f3c1d600d5e2c2cee39c5280ece73e1c8
Your Name <you@example.com>
2025-01-14 14:05:27 +0530

    Added mechanism to revoke old token whenever new is created

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // Import Modueles
 import express from "express"
 import connectDB from "./db.js"
-import printLog from "./utils/printLog.js"
 import authRoute from "./routes/authentication.route.js"
 import contestRoute from "./routes/contest.route.js"
+import printLog from "./utils/printLog.js"
 import declarePrototypes from "./utils/prototypes.js"
+
 const app = express()
 const PORT = process.env.PORT || 3000
 
--- utils/createJWT.js
+++ utils/createJWT.js
@@ -1,5 +1,9 @@
 import jwt from "jsonwebtoken";
+import { v4 as uuidv4 } from 'uuid';
+import redisClient from "../redis.js";
 
 export default ( role, _id ) => {
-    return jwt.sign({ role, _id }, process.env.JWT_SECRET_KEY, { expiresIn: "1d" });
+    let tokenId = uuidv4();
+    redisClient.set(`tokens:${role}_${_id}`, tokenId);
+    return jwt.sign({ role, _id, tokenId }, process.env.JWT_SECRET_KEY, { expiresIn: "1d" });
 };
